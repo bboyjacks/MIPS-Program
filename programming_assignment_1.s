08b0629353a2b804faa66f6bed80d17aad8e4637
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro-2.local>
2018-10-02 09:39:00 -0700

    able to take input from user and print out input

--- programming_assignment_1.s
+++ programming_assignment_1.s
@@ -17,6 +17,13 @@ main:
     la		$a0, int_str
     syscall
 
+    li		$v0, 5		        # $v0 = 5 : code to read integer
+    syscall    
+    
+    move 	$t0, $v0		    # $t0 = $v0 : move the value read into $t0
+    li		$v0, 1		        # $v0 = 1   : code to print integer
+    move 	$a0, $t0		    # $a0 = $t0 : move the value in $t0 to $a0 to be printed
+    syscall
+
     li $v0, 10                  # end program
     syscall
-    
