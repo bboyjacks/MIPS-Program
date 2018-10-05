983443ec16d59eb35c66eaec84be991ed101222f
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2018-10-04 18:10:22 -0700

    added .data section

--- programming_assigntment_2.s
+++ programming_assigntment_2.s
@@ -4,3 +4,10 @@
 # Description:
 #   This program waits for input of integer, operation, and integer
 #   then evaluates the entered statement and prints them
+
+.data
+
+int_str:        .asciiz "int: "
+op_str:         .asciiz "op: "
+goodbye_message .asciiz "Goodbye"
+input_op:       .space 4
