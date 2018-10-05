629397536fedf2f83b479fb916725819f613ac42
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2018-10-04 18:08:23 -0700

    restarted fresh

---
+++ programming_assigntment_2.s
@@ -0,0 +1,6 @@
+# Lester Dela Cruz
+# CDA3101
+# programming_assignment_1.s
+# Description:
+#   This program waits for input of integer, operation, and integer
+#   then evaluates the entered statement and prints them
